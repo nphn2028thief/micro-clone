67ed85ec8438dacfcf82aaca05c74be13a2e1a12
Nguyen Phan Hoai Nhan <nphn2082thief@gmail.com>
2024-03-03 08:21:18 +0700

    Init project

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  providers: [
+    {
+      domain: "https://sought-mule-59.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
